1eb20bb01f30942ba4a3310a22343029d2ac88cd
zubowaantonina <zubowaantonina@yandex.ru>
2022-08-31 15:57:10 +0300

    fix: navbar scroll

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (this.scrollY > 1) {
+    navbar.classList.add("navbar-light");
+  } else {
+    navbar.classList.remove("navbar-light");
+  }
+});
