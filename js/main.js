b5ce8607111c58d4cf1a06f39b898490d4a8de5b
zubowaantonina <zubowaantonina@yandex.ru>
2022-09-07 00:18:42 +0300

    add: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 const navbar = document.querySelector(".navbar");
-const logo= document.querySelector('.logo-svg use')
+const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
-  
-    logo.href.baseVal='img/sprite.svg#logo';
+
+    logo.href.baseVal = "img/sprite.svg#logo";
   } else {
     navbar.classList.remove("navbar-light");
-    logo.href.baseVal='img/sprite.svg#logo-light';
+    logo.href.baseVal = "img/sprite.svg#logo-light";
   }
+});
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.toggle("is-open");
 });
