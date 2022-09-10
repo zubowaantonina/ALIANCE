2ffc6d4bd0905513445583718ff770af9f76b695
zubowaantonina <zubowaantonina@yandex.ru>
2022-09-10 17:10:58 +0300

    fix: logo swither

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const navbar = document.querySelector(".navbar");
-const logo = document.querySelector(".logo-svg use");
+const logoLight = document.querySelector(".logo-light");
+const logo = document.querySelector(".logo");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
-  logo.href.baseVal = "img/sprite.svg#logo";
+ logo.style.display='block';
+ logoLight.style.display='none';
 };
 const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
-  logo.href.baseVal = "img/sprite.svg#logo-light";
+  logo.style.display='none';
+  logoLight.style.display='block';
 };
 //функция открывания меню
 const openMenu = (event) => {
