85f376f63691c7a93b55606acfd93899be223d94
zubowaantonina <zubowaantonina@yandex.ru>
2022-10-15 22:04:00 +0300

    fix: link header-psge.php

--- header-page.php
+++ header-page.php
@@ -55,7 +55,7 @@
                 <a href="#" class="mobile-menu-link">Новости</a>
             </li>
             <li class="mobile-menu-nav-item">
-                <a href="#" class="mobile-menu-link">Контакты</a>
+                <a href="./contacts.php" class="mobile-menu-link">Контакты</a>
             </li>
         </ul>
 
